c458bbaad8dbe43e8a9e9657670d637f82a93d86
douhongchen <douhongchen@kingsoft.com>
2021-03-04 18:27:51 +0800

    update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,5 @@ REPO_SERVER=hub.docker.com
 IMAGE_NAME=backtrader:${VERSION}
 REMOTE_IMAGE_NAME=${REPO_SERVER}/repository/docker/douhongchen/${IMAGE_NAME}
 
-docker build -t $IMAGE_NAME  -f Dockerfile .
+docker build -t $IMAGE_NAME  -f Dockerfile ..
 # docker push ${REMOTE_IMAGE_NAME}
